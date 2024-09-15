fb4ba255b33c55aa0cca1139b6a2c9e5e8d8b569
vadyacodit <vadimlipatov786@gmail.com>
2024-09-15 22:18:02 +0200

    Removed unnecessary classes, combined in 1 file

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
-from parser import Parser
+import requests
+
+from typing import *
+from fake_useragent import FakeUserAgent
+
+
+def parse_data(url, init_date, final_date) -> Dict:
+    headers = {
+        'user-agent': FakeUserAgent().random,
+    }
+
+    params = {
+        'game_id': 'BONO',
+        'celebrados': 'true',
+        'fechaInicioInclusiva': init_date,
+        'fechaFinInclusiva': final_date,
+    }
+
+    response = requests.get(
+        'https://www.loteriasyapuestas.es/servicios/buscadorSorteos',
+        params=params,
+        headers=headers,
+    )
+
+    result_json = response.json()
+    result_dict = {}
+    for result in result_json:
+        numbers = [num.strip() for num in result['combinacion'].split('-')][:5]
+        for i in range(len(numbers)):
+            numbers[i] = int(numbers[i])
+        result_dict[result['fecha_sorteo'][:10]] = numbers
+
+    return result_dict
+
+
+def create_matrix(result_dict: Dict) -> List:
+    matrix = []
+    for val in result_dict.values():
+        matrix.append(val)
+
+    return matrix
+
 
 link = 'https://www.loteriasyapuestas.es/es/resultados/bonoloto'
-parser = Parser(source_url=link, init_date='20240815', final_date='20240915')
-
-parsed_data = parser.parse_data()
+parsed_data = parse_data(url=link, init_date='20240815', final_date='20240915')
 print(parsed_data)
+
+print(create_matrix(parsed_data))
